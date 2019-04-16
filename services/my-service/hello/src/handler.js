3bc42eb98044e30adc8aff5f4f0453227d5e3f78
lakatosandrei <lakatosandrei@gmail.com>
2019-04-16 08:47:26 +0300

    chore(hello): added some console logs

--- services/my-service/hello/src/handler.js
+++ services/my-service/hello/src/handler.js
@@ -7,6 +7,10 @@ const vars = JSON.parse(fs.readFileSync('.vars.json', 'utf8'));
 
 export function handler(...args) {
   event(vars, (req, res) => {
+    console.log('Hello there');
+
+    console.log(req.query.name);
+
     res.json(200, {
       name: `Your name : ${req.query.name}`,
       message: req.body.message || 'hello world !!!'
